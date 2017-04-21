346d8a09e2861e10d925a0272706daeb84cd39c9
yongbiaoshi <shyb0904@163.com>
2017-04-21 20:32:07 +0800

    添加日志拦截器

---
+++ src/main/java/com/example/demo/mb/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.mb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.example.demo.mb.interceptor.LoggingInterceptor;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+        registry.addInterceptor(loggingInterceptor());
+    }
+
+    @Bean
+    public LoggingInterceptor loggingInterceptor() {
+        return new LoggingInterceptor();
+    }
+}
