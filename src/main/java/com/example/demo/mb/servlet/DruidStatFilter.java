a54ac60a18975d203d9b4e8902a9590ed51d872b
yongbiaoshi <shyb0904@163.com>
2017-04-24 22:26:03 +0800

    format

--- src/main/java/com/example/demo/mb/servlet/DruidStatFilter.java
+++ src/main/java/com/example/demo/mb/servlet/DruidStatFilter.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebInitParam;
 import com.alibaba.druid.support.http.WebStatFilter;
 
 @WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*", //
-        initParams = { //初始化参数
+        initParams = { // 初始化参数
                 @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
         })
 public class DruidStatFilter extends WebStatFilter {
