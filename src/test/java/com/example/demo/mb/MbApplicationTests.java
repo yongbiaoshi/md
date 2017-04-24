757b0c54b389d098fe763477fbd99f4e58a4bb54
yongbiaoshi <shyb0904@163.com>
2017-04-24 21:27:59 +0800

    修改配置

--- src/test/java/com/example/demo/mb/MbApplicationTests.java
+++ src/test/java/com/example/demo/mb/MbApplicationTests.java
@@ -1,15 +1,15 @@
 package com.example.demo.mb;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class MbApplicationTests {
 
-	@Test
+//	@Test
 	public void contextLoads() {
 	}
 
