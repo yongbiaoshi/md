ae9f390c9d8bbf4d632a377ea33b2a20c5b662c8
yongbiaoshi <shyb0904@163.com>
2017-05-04 21:42:22 +0800

    调用service 示例

--- src/main/java/com/example/demo/mb/controller/HomeController.java
+++ src/main/java/com/example/demo/mb/controller/HomeController.java
@@ -2,18 +2,28 @@ package com.example.demo.mb.controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.demo.mb.log.RequestIdManager;
+
+import com.example.demo.mb.service.DemoService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 public class HomeController {
+    
+    @Resource
+    private DemoService demoService;
+    
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String home() {
-        log.info("访问首页。RequestId：{}", RequestIdManager.getId());
+        log.info("访问首页。");
+        demoService.doSomething();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String now = sdf.format(new Date());
         return "现在时间：" + now;
